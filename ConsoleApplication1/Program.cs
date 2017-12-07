7e038e3959283946bdff7a1df32e662caa4b3230
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2017-12-07 16:50:18 +1100

    Initial checkin

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It's a test really");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
